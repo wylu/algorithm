2ebdd41fdc5853a128c5dd0380be410737d6d209
wylu <158677478@qq.com>
2020-09-17 20:15:13 +0800

    union find

---
+++ 数据结构/关系结构/并查集/并查集.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   并查集.py
+@Time    :   2020/09/17 15:27:19
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+
+
+class UnionFind:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
