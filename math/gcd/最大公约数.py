b3cebb0695a36668d33570fb4961120df260b774
wylu <158677478@qq.com>
2020-10-26 23:21:42 +0800

    gcd

---
+++ math/gcd/最大公约数.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   最大公约数.py
+@Time    :   2020/10/26 23:18:46
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+
+
+def gcd(x: int, y: int) -> int:
+    return x if y == 0 else gcd(y, x % y)
+
+
+if __name__ == "__main__":
+    print(gcd(12, 4))
+    print(gcd(12, 3))
