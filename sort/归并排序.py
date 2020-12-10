63a88793d9cf5e5f1a5ef9025ced7d154395eede
wylu <158677478@qq.com>
2020-12-10 23:15:40 +0800

    归并排序

--- sort/归并排序.py
+++ sort/归并排序.py
@@ -32,25 +32,24 @@ from typing import List
 def merge(nums: List[int], left: int, mid: int, right: int) -> None:
     tmp = [0] * (right - left + 1)
     i, j, k = left, mid + 1, 0
-    while i <= mid and j <= right:
-        if nums[i] <= nums[j]:
-            tmp[k] = nums[i]
-            i, k = i + 1, k + 1
-        else:
+    while i <= mid or j <= right:
+        if i > mid:
             tmp[k] = nums[j]
-            j, k = j + 1, k + 1
-
-    while i <= mid:
-        tmp[k] = nums[i]
-        i, k = i + 1, k + 1
-
-    while j <= right:
-        tmp[k] = nums[j]
-        j, k = j + 1, k + 1
+            j += 1
+        elif j > right:
+            tmp[k] = nums[i]
+            i += 1
+        else:
+            if nums[i] <= nums[j]:
+                tmp[k] = nums[i]
+                i += 1
+            else:
+                tmp[k] = nums[j]
+                j += 1
+        k += 1
 
     for i in range(k):
-        nums[left] = tmp[i]
-        left += 1
+        nums[left + i] = tmp[i]
 
 
 def merge_sort(nums: List[int], left: int, right: int) -> None:
